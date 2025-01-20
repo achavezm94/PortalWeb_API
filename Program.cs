72b28721aeef25246fd8b2f9330ea883d14cc948
achavezm94 <109546382+achavezm94@users.noreply.github.com>
2025-01-20 09:24:32 -0500

    Intento de subir a azure (falla de login)

--- Program.cs
+++ Program.cs
@@ -105,10 +105,10 @@ builder.Services.AddCors(opt =>
     opt.AddPolicy(name: reglasCors, builder =>
     {
         builder.WithOrigins(
-        //"https://sfifront.azurewebsites.net"
-        "http://192.168.55.236:2251",
-        "http://192.168.55.19:2253",
-        "http://192.168.55.212:2251"
+        "https://sfifront.azurewebsites.net"
+        //"http://192.168.55.236:2251",
+        //"http://192.168.55.19:2253",
+        //"http://192.168.55.212:2251"
     )
    .AllowAnyHeader()
    .AllowAnyMethod()
@@ -121,10 +121,10 @@ var webSocketOptions = new WebSocketOptions
     KeepAliveInterval = TimeSpan.FromSeconds(120)
 };
 
-//webSocketOptions.AllowedOrigins.Add("https://sfifront.azurewebsites.net");
-webSocketOptions.AllowedOrigins.Add("http://192.168.55.236:2251");
-webSocketOptions.AllowedOrigins.Add("http://192.168.55.19:2253");
-webSocketOptions.AllowedOrigins.Add("http://192.168.55.212:2251");
+webSocketOptions.AllowedOrigins.Add("https://sfifront.azurewebsites.net");
+//webSocketOptions.AllowedOrigins.Add("http://192.168.55.236:2251");
+//webSocketOptions.AllowedOrigins.Add("http://192.168.55.19:2253");
+//webSocketOptions.AllowedOrigins.Add("http://192.168.55.212:2251");
 
 var app = builder.Build();
 
